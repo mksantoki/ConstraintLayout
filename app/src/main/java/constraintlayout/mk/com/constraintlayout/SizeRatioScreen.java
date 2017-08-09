3c1176f7cfaf174522b6aaac5d707b54b56bd7b3
Pappsmauliksantoki <maulik.santoki@ccs.sg>
2017-08-09 18:29:12 +0530

    Initial commit

---
+++ app/src/main/java/constraintlayout/mk/com/constraintlayout/SizeRatioScreen.java
@@ -0,0 +1,24 @@
+package constraintlayout.mk.com.constraintlayout;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mauliksantoki on 9/8/17.
+ */
+
+
+
+
+
+public class SizeRatioScreen extends AppCompatActivity {
+
+    //TODO  1) For Size Ration Property Need One (x/y) Is 0dp And other Has Height
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.size_ratio_screen);
+    }
+}
